caa079c0572c156f5b1a47e8e15826367f66d024
Anichitoaie Syraj <syraj_syraj@yahoo.com>
2025-08-19 09:17:59 +0300

    aaa

--- src/test/java/ApiTesting/HTTP_Requests.java
+++ src/test/java/ApiTesting/HTTP_Requests.java
@@ -19,7 +19,7 @@ public class HTTP_Requests {
 
     @Test
     void getUsers(){
-        //some comment9
+        //some comment
         given()
                 .header("x-api-key","reqres-free-v1")
                 .when()
